35f724fd37368e93b0c8a3e52e25e54915d6ba16
Daniel Costa <daniel.dc697@gmail.com>
2025-04-04 22:03:13 -0300

    ajustando a reponsividade

--- src/Components/Banner/styles.js
+++ src/Components/Banner/styles.js
@@ -1,20 +1,19 @@
 import styled from "styled-components";
-
 const BannerContainer = styled.section`
   width: 100vw;
   display: flex;
-  justify-content:space-between;
+  justify-content: space-between;
   align-items: center;
   padding: 3rem 10rem;
   background-color: #041833;
-  min-height:40vh;
-  
+  min-height: 50vh;
+  max-height: 50vh;
+
   @media screen and (max-width: 768px) {
     min-height: 20vh;
     padding: 3rem 2rem;
     gap: 2rem;
   }
-
 `;
 
 const ConteinerDescricao = styled.div`
@@ -24,30 +23,25 @@ const ConteinerDescricao = styled.div`
   flex-direction: column;
   gap: 1rem;
 
-
   h1 {
-    font-size: 5rem;
+    font-size: 6rem;
     font-weight: 700;
   }
 
   p {
-    font-size:1.5rem;
+    font-size: 2rem;
     font-weight: 400;
-
-    
   }
 
   @media screen and (max-width: 768px) {
-    
     h1 {
       font-size: 2rem;
     }
 
     p {
-      font-size: .75rem;
+      font-size: 0.75rem;
       width: 100%;
-    }   
-    
+    }
   }
 
   @media screen and (max-width: 480px) {
@@ -58,29 +52,26 @@ const ConteinerDescricao = styled.div`
     p {
       font-size: 0.8rem;
       width: 100%;
-    }   
+    }
   }
 `;
 
 const ConteinerImg = styled.div`
-width: 30%;
-display: flex;
-justify-content: center;
-align-items: center;
-@media screen and (max-width: 768px) {
+  width: 40%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  @media screen and (max-width: 768px) {
     width: 50%;
   }
 
   @media screen and (max-width: 480px) {
     width: 70%;
-  } 
+  }
 `;
-    
+
 const Img = styled.img`
-    width: 50%;
-    height: auto;
-    object-fit: cover;
-   
+max-width: 70%;
 `;
 
 export { BannerContainer, ConteinerDescricao, ConteinerImg, Img };
